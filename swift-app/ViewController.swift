1971bbd20dc75914efd44c4ca0ffdde2c7455eb0
Hendrik Van Heuverswyn <appelken@Appelken.local>
2017-12-25 17:10:52 +0100

    Initial Commit

---
+++ swift-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-app
+//
+//  Created by Hendrik Van Heuverswyn on 25/12/2017.
+//  Copyright © 2017 Hendrik Van Heuverswyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

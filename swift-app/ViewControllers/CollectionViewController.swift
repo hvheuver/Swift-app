ee9b152788f028fb929c91fa2087253cd1f5d5f7
hvheuver <hendrik.vanheuverswyn.w3917@student.hogent.be>
2017-12-28 23:31:07 +0100

    Initial commit - login/UICollectionView

---
+++ swift-app/ViewControllers/CollectionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CollectionViewController.swift
+//  swift-app
+//
+//  Created by Hendrik Van Heuverswyn on 28/12/2017.
+//  Copyright © 2017 Hendrik Van Heuverswyn. All rights reserved.
+//
+
+import Foundation

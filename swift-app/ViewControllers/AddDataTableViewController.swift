4aaf7bf57626609e83dafe08fc2f9cd4eceee138
hvheuver <hendrik.vanheuverswyn.w3917@student.hogent.be>
2017-12-30 19:44:28 +0100

    AddDataTableView added + layout

---
+++ swift-app/ViewControllers/AddDataTableViewController.swift
@@ -0,0 +1,15 @@
+import Foundation
+import Firebase
+import UIKit
+import FirebaseDatabase
+
+class AddDataTableViewController: UITableViewController {
+    @IBOutlet weak var hoogvogels: UITextField!
+    @IBOutlet weak var zijvogels: UITextField!
+    @IBOutlet weak var kalleVogels: UITextField!
+    @IBOutlet weak var kleineVogels: UITextField!
+
+    @IBAction func save() {
+        //TODO
+    }
+}
